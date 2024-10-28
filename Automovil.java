74989c87a2f01b6bb12ffde32764e36c49f279c3
Miguezx <140022542+Miguezx@users.noreply.github.com>
2024-10-27 20:32:06 -0500

    Update_Automovil.java

--- Automovil.java
+++ Automovil.java
@@ -61,5 +61,26 @@ public class Automovil {
     }
 }
 
+//Main
+public class Main {
+    public static void main(String[] args) {
+        Automovil bmw = new Automovil();
+        bmw.setMarca("BMW");
+        bmw.setModelo("i320");
+        bmw.setColor("Rosa");
+        bmw.setCilindraje(42.5);
+
+        Automovil bmw2 = new Automovil("Mercedes", "i320");
+        bmw2.setColor("Rosa");
+        bmw2.setCilindraje(42.5);
+
+        bmw.verDetalle();
+        bmw2.verDetalle();
+
+        System.out.println("-");
+        System.out.println(bmw.equals(bmw2));
+    }
+}
+
 
 
