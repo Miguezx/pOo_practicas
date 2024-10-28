61df50f5c53a2a798fc5a4feb97719662e89f312
Miguezx <140022542+Miguezx@users.noreply.github.com>
2024-10-27 20:32:55 -0500

    Update Flor.java

--- Flor.java
+++ Flor.java
@@ -39,4 +39,17 @@ public class Flor {
 }
 
 
+//Main
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Flor rosa = new Flor("rosa", "rojo", 50);
+        Flor girasol = new Flor("girasol", "amarillo", 150);
+        Flor tulipan = new Flor("tulipan", "verde", 67.5);
+        rosa.mostrarDetalles();
+        girasol.mostrarDetalles();
+    }
+}
+
 
